94349f80ef78d1e6ec47f1dc37009b12ecc7c4e6
kkshimamura <kkshimamura@30c2104e-ad3a-11de-a101-efa912109f1f>
2009-10-08 21:53:54 +0000

    preference observer... maybe?

--- chrome/content/Preferences.js
+++ chrome/content/Preferences.js
@@ -1,33 +1,60 @@
-var Preferences = function() {
-	this.parserOn = true;
-	this.myLocation = null;
-	this.AddressListDialogOn = null;
+var Preferences = {
+	parserOn: true,
+	myLocation: null,
+	AddressListDialogOn: null,
 	
-	this.toString = function() {
+	toString: function() {
 		return this.getStreet() + ', ' + this.getCity() + ', ' + this.getState() + ' ' + this.getZip();
-	};
+	},
 	
-	this.getParserOn = function() {
+	getParserOn: function() {
 		return this.parserOn;
-	};
+	},
 	
-	this.getMyLocation = function() {
+	getMyLocation: function() {
 		return this.myLocation;
-	};
+	},
 	
-	this.getAddressDialogListOn = function() {
+	getAddressDialogListOn: function() {
 		return this.AddressListDialogOn;
-	};
+	},
 	
-	this.setAddressDialogListOn = function(on) {
+	setAddressDialogListOn: function(on) {
 		this.AddressListDialogOn = on;
-	};
+	},
 	
-	this.setParserOn = function(on) {
+	setParserOn: function(on) {
 		this.parserOn = on;
-	};
+	},
 	
-	this.setMyLocation = function(a) {
+	setMyLocation: function(a) {
 		this.myLocation = a;
-	};
+	},
+	
+	observe: function(subject, topic, data) {
+		if (topic != 'nsPref:changed')
+			return null;
+		var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService);
+		prefs = prefs.getBranch('extensions.simplemap.');
+		switch (data) {
+			case 'location':
+				Preferences.setMyLocation(prefs.getCharPref('location'));
+				break;
+			case 'enableList':
+				Preferences.setAddressDialogListOn(prefs.getBoolPref('enableList'));
+				break;
+			case 'enableParser':
+				Preferences.setParserOn(prefs.getBoolPref('enableParser'));
+				break;
+		}
+	}
 };
+
+
+var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService);
+prefs = prefs.getBranch('extensions.simplemap.');
+prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+prefs.getCharPref('charPrefName');
+prefs.getBoolPref('boolPrefName');
+prefs.addObserver('', Preferences, false);
+delete prefs;
