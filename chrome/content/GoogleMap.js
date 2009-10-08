b53f3da41199bf308a7692119d88823fd46a1f56
kkshimamura <kkshimamura@30c2104e-ad3a-11de-a101-efa912109f1f>
2009-10-08 21:22:37 +0000

    added direction link to GoogleMap

--- chrome/content/GoogleMap.js
+++ chrome/content/GoogleMap.js
@@ -6,5 +6,11 @@ var GoogleMap = {
 			return 'http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=' + addr;
 	},
 	getLinkToAddressDirection: function(destination, location) {
+		// http://maps.google.com/maps?f=d&source=s_d&saddr=417+N+Tatnall+St,+Wilmington,+DE+19801&daddr=427+N+Tatnall+St,+Wilmington,+DE+19801
+		if (destination instanceof Address)
+			destination = destination.toString();
+		if (location instanceof Address)
+			location = location.toString();
+		return 'http://maps.google.com/maps?f=d&source=s_d&saddr=' + location + '&daddr=' + destination;
 	}
 };
