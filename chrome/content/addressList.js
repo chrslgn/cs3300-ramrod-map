65aa41337ba0737d1cb21888d6b41fd301e50735
shortrocket <shortrocket@30c2104e-ad3a-11de-a101-efa912109f1f>
2009-10-10 18:59:19 +0000

    - Populate address list from controller - Add event listeners so the buttons will work

--- chrome/content/addressList.js
+++ chrome/content/addressList.js
@@ -4,4 +4,28 @@ window.addEventListener('load', function() {
 	var doc = window.opener.content.document;
 	var c = doc.SimpleMap;
 	Components.utils.reportError(c.addresses); // DEBUG
+	
+	var addressArray = c.addresses;
+	var addrArrayLen = addressArray.length;
+	
+	for(i = 0; i < addrArrayLen; i++) {
+		var addrList = document.getElementById("address-list");
+		addrList.appendItem(addressArray[i].toString());
+	}
+	
+	document.getElementById("show-map").addEventListener("oncommand", function() {
+		var addr = document.getElementById("address-list").getSelectedItem(0).getAttribute("label");
+		var link = GoogleMap.getLinkToAddress(addr);
+		
+		gBrowser.selectedTab = gBrowser.addTab(link);
+	},false);
+	
+	document.getElementById("show-directions").addEventListener("oncommand", function() {
+		
+		var addr = document.getElementById("address-list").getSelectedItem(0).getAttribute("label");
+		var link = GoogleMap.getLinkToAddressDirection(addr, c.getPreferences().getMyLocation());
+		
+		gBrowser.selectedTab = gBrowser.addTab(link);
+	}, false);
+	
 }, false);
