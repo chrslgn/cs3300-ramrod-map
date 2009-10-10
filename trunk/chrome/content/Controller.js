32367e34095472f3de44dc0d6e564e5328c3db3b
kkshimamura <kkshimamura@30c2104e-ad3a-11de-a101-efa912109f1f>
2009-10-10 04:47:41 +0000

    split loading code from Controller

--- trunk/chrome/content/Controller.js
+++ trunk/chrome/content/Controller.js
@@ -24,27 +24,3 @@ var Controller = function(bodyDOM) {
 	this.newWindow = function(){
 	};
 };
-
-window.addEventListener('load', function() {
-	var appcontent = document.getElementById('appcontent');
-	if (!appcontent)
-		return null;
-	
-	var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService);
-	prefs = prefs.getBranch('extensions.simplemap.');
-	prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
-	Preferences.setMyLocation(prefs.getCharPref('location'));
-	Preferences.setAddressDialogListOn(prefs.getBoolPref('enableList'));
-	Preferences.setParserOn(prefs.getBoolPref('enableParser'));
-	
-	appcontent.addEventListener('DOMContentLoaded', function(e) {
-		var doc = e.originalTarget;
-		if (doc.body === undefined)
-			return null;
-		var c = new Controller(doc.body);
-		prefs.addObserver('', c.preferences, false);
-	//	Components.utils.reportError(c.getPreferences()); // DEBUG
-		if (c.getPreferences().getParserOn())
-			c.parsePage();
-	}, true);
-}, false);
