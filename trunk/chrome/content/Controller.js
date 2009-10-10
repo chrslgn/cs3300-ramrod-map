7c218713a4ad011ed0c1061d7eb9c440dc5d5642
kkshimamura <kkshimamura@30c2104e-ad3a-11de-a101-efa912109f1f>
2009-10-10 05:59:40 +0000

    progress on direction

--- trunk/chrome/content/Controller.js
+++ trunk/chrome/content/Controller.js
@@ -19,10 +19,23 @@ var Controller = function(bodyDOM) {
 	};
 	this.mapAddress = function(address) {
 		var addr = Address.fromString(address);
+		
+		// let Google handle it if we can't parse it
+		if (!addr)
+			addr = address;
+		
 		var link = GoogleMap.getLinkToAddress(addr);
 		gBrowser.selectedTab = gBrowser.addTab(link);
 	};
 	this.showDirections = function(string) {
+		var addr = Address.fromString(string);
+		
+		// let Google handle it if we can't parse it
+		if (!addr)
+			addr = string;
+		
+		var link = GoogleMap.getLinkToAddressDirection(addr, this.getPreferences().getMyLocation());
+		gBrowser.selectedTab = gBrowser.addTab(link);
 	};
 	this.newWindow = function(){
 	};
